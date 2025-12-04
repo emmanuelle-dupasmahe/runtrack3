435aa32fcf519438ead8076fcffc18eaa00dbffa
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-12-04 13:32:04 +0100

    fonction qui affiche les jours de la semaine dans la console

--- Jour01/Job05/index.php
+++ Jour01/Job05/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Les Années grégoriennes</title>
+    <title>Les jours de la semaine</title>
     <!--
     onverture balise avec attribut inline
     <script src="./script.js">
--- Jour01/Job05/script.js
+++ Jour01/Job05/script.js
@@ -1,8 +1,8 @@
-function bissextile(annee) {
-    let result = (annee % 4 === 0 && annee % 100 != 0) || annee % 400 === 0 
-        ? `${annee} est une année bissextile` 
-        : `${annee} n'est pas une année bissextile`;
-    console.log(result);
+function afficherjourssemaines() {
+    const semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi"];
+    for (let i = 0; i < semaine.length; i++) {
+    console.log(semaine[i]);
+    }
 }
+afficherjourssemaines();
 
-bissextile(2100);
