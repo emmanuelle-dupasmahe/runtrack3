6ca7bffc4dc1b668e5ce495528d744b49b224ce8
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-12-04 15:51:11 +0100

    une fonction tri qui tri un tableau de nombres on lui indique en mode ascendant et ascendant

---
+++ Jour01/Job09/script.js
@@ -0,0 +1,27 @@
+function tri(numbers, order) {
+  if (order !== 'asc' && order !== 'desc') {
+    console.error("Erreur : Le paramètre 'order' doit être 'asc' ou 'desc'.");
+    return numbers;
+  }
+
+  
+  const tableauTrie= numbers.sort((a, b) => {
+
+    if (order === 'asc') {
+      return a - b;
+    } else { 
+      return b - a;
+    }
+  });
+
+  
+  console.log(`Tableau trié en ordre ${order === 'asc' ? 'ascendant' : 'décroissant'} :`);
+  console.log(tableauTrie);
+
+  return tableauTrie;
+}
+
+const monTableau = [4, 1, 8, 5, 2, 9, 3, 7, 6];
+
+const tableauAscendant = tri([monTableau], 'asc');
+const tableauDescendant = tri([monTableau], 'desc');
