93c0ad8131912a7970f0bde001ab7e02c66db3c1
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-12-04 13:57:32 +0100

    une fonction fizzbuzz qui affiche les nombres de 1 à 151 si les nombres sont multiples de 3 et 5 alors la fonction affiche Fizzbuzz si les nombres sont multiples de 3 elle affiche Fizz et de 5 elle affiche Buzz dans la console

--- Jour01/Job06/script.js
+++ Jour01/Job06/script.js
@@ -1,8 +1,17 @@
-function afficherjourssemaines() {
-    const semaine = ["lundi", "mardi", "mercredi", "jeudi", "vendredi"];
-    for (let i = 0; i < semaine.length; i++) {
-    console.log(semaine[i]);
+function fizzbuzz() {
+    for (let i = 1; i <= 151; i++) {
+        if (i % 3 === 0 && i %5 === 0){
+            console.log("Fizzbuzz");
+        }
+        else if (i % 3 === 0){
+            console.log("Fizz");
+        }
+        else if (i % 5 === 0){
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
     }
 }
-afficherjourssemaines();
-
+fizzbuzz();
