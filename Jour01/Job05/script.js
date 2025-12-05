02f7d4ee39492cbababddcae90933a5a78f53a71
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-12-05 09:24:53 +0100

    correction j'ai ajouté samedi et dimandhe aux jours de la semaine

--- Jour01/Job05/script.js
+++ Jour01/Job05/script.js
@@ -1,5 +1,5 @@
 function afficherjourssemaines() {
-    const jourssemaines = ["lundi", "mardi", "mercredi", "jeudi", "vendredi"];
+    const jourssemaines = ["lundi", "mardi", "mercredi", "jeudi", "vendredi","samedi","dimanche"];
     for (let i = 0; i < jourssemaines.length; i++) {
     console.log(jourssemaines[i]);
     }
