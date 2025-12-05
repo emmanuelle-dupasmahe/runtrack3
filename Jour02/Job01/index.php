6be3c1a5235d332fecb2776a64ffc2f7c0d5a9c0
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-12-05 13:49:08 +0100

    début du Job01 affiche une citation

---
+++ Jour02/Job01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>affiche une citation</title>
+     <script src="./script.js"></script> 
+</head>
+<body>
+    <div id ="article">La vie a beaucoup plus d’imagination que nous.</div>
+     <button onclick="toggleBoite()">Afficher/Masquer</button>
+</body>
+</html>
