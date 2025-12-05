61dbf202072b1e5861ecf538ca19053620ce79d3
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-12-05 14:13:50 +0100

    début du code pour faire une fonction showhide() qui affiche ou cache un article

---
+++ Jour02/Job02/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>afficher et masquer</title>
+     <script src="./script.js"></script> 
+</head>
+<body>
+    <article id ="citation">L'important n'est pas la chute, mais l'atterrissage.</article>
+    <div id ="button">Afficher/Masquer</div>
+    <button onclick="showhide()">Afficher/Masquer</button>
+
+</body>
+</html>
