28a8702564b61b8b948185d4072b0f45c3b1f58c
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-12-05 15:13:39 +0100

    fonction addone qui ajoute 1 à chaque clic

--- Jour02/Job03/script.js
+++ Jour02/Job03/script.js
@@ -1,13 +1,8 @@
-let compteur = 0;
-function incrementer () {
-    compteur++;
-    document.getElementById('compteur').textContent = compteur;
-}
-function decrementer () {
-    compteur--;
-    document.getElementById('compteur').textContent = compteur;
-}
-function reset () {
-    compteur = 0;
-    document.getElementById('compteur').textContent = compteur;
-}
+const compteur = document.getElementById('compteur');
+        function addone() {
+            let currentValue = compteur.textContent;
+            let numericValue = parseInt(currentValue);
+            numericValue = numericValue + 1; 
+            compteur.textContent = numericValue;
+        }
+        document.getElementById('button').addEventListener('click', addone);
