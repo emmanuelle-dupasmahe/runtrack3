fb606b1bbb678e91f6fd2bb41d340603325c8c97
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-12-16 09:19:53 +0100

    ajout d'une modale quand on clique sur commander un papillon

--- Jour06/Job02/index.php
+++ Jour06/Job02/index.php
@@ -163,7 +163,7 @@
                     <hr class="mt-4">
                 </div>
             </div>
-                    </div>
+    </div>
 
     <div class="grey-progress-container mt-4 mb-5">
             <div class="progress mb-2" style="height: 8px;"> 
@@ -177,6 +177,24 @@
     <button class="btn btn-primary confidentiality-btn">
         <i class="fas fa-star me-2"></i> ⚙️ Confidentialité
     </button>
+
+    <div class="modal fade" id="papillonModal" tabindex="-1" aria-labelledby="papillonModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+    <div class="modal-content">
+    <div class="modal-header">
+    <h5 class="modal-title" id="papillonModalLabel">Commander votre papillon</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+    <div class="modal-body">
+    <p>Veuillez confirmer l'envoi de votre bon de commande pour un papillon (attention : livraison non garantie).</p>
+</div>
+    <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+    <button type="button" class="btn btn-primary">Confirmer la commande</button>
+</div>
+</div>
+</div>
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
